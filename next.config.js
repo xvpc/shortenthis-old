bfe8c44aa359f2ffb2b870f0328fb33bc7177a2d
Viper <viperxx100@gmail.com>
2023-05-04 16:51:34 +0300

    #

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   experimental: { runtime: 'edge'}
-// }
-
-// module.exports = nextConfig
 /** @type {import('next').NextConfig} */
-const nextConfig = { 
-  experimental: { runtime: 'edge' },  
-  reactStrictMode: true,  
-  swcMinify: true,
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: { runtime: 'edge'}
 }
+
 module.exports = nextConfig
